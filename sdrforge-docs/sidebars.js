a55b373bcea491aecb521eeecc31e533112f531a
Cour1erNVs <ivanelicasa@gmail.com>
2026-06-17 13:15:38 -0700

    Basic doc changes

---
+++ sdrforge-docs/sidebars.js
@@ -0,0 +1,18 @@
+module.exports = {
+  tutorialSidebar: [
+    'Overview',
+    'Installation',
+    'Troubleshooting',
+    'Contributors',
+    {
+      type: 'category',
+      label: 'Lab Walkthroughs',
+      items: [
+        'Lab-Walkthroughs/GPS-Spoof',
+        'Lab-Walkthroughs/IMSI-Catcher',
+        'Lab-Walkthroughs/ADSB',
+        'Lab-Walkthroughs/Doorbell-Relay',
+      ],
+    },
+  ],
+};
